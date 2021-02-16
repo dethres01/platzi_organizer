70df52a8dd024b9c576c9a8141d3c5e410938930
dethres01 <diegoa.preciado@alumnos.udg.mx>
2021-02-16 11:03:53 -0600

    Finally understood how to call the sass files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,19 +12,8 @@ require("@nathanvda/cocoon")
 
 import "roboto-fontface"
 import "bootstrap"
-import "../stylesheets/application"
+import "../src/stylesheets/application"
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-        return new bootstrap.Popover(popoverTriggerEl)
-    })
-
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-});
 //import "../assets/stylesheets/application"
 //import 'cocoon'
 // Uncomment to copy all static images under ../images to the output folder and reference
