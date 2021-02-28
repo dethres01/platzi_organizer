a1ba0aaee995e558e06842fff2a993ec657cc168
dethres01 <diegoa.preciado@alumnos.udg.mx>
2021-02-28 09:43:27 -0600

    Done with system test for now, coffeescript setup

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,18 +12,26 @@ RSpec.describe "Tasks", type: :system do
   end
   describe 'POST /tasks/new' do
     let!(:category){create :category}
+    let!(:participant) {create :user}
     it 'has the correct title' do
       visit '/tasks/new'
       expect(page).to have_content 'Nueva Tarea'
     end
-    it 'has to fill in',js:true do
+    it 'Creates a new task',js:true do
       visit '/tasks/new'
       fill_in('task[name]', with: 'Tarea Capybara 1')
       fill_in('task[due_date]', with: Date.today+5.days)
       fill_in('task[description]', with: 'descripcion Tarea Capybara 1')
       select(category.name,from: 'task[category_id]')
       click_link('Agregar un participante')
-      binding.pry
+      xpath ='/html/body/div/div[2]/div/div/form/div[1]/div[4]/div[1]'
+      within(:xpath,xpath) do
+        select(participant.email,from: 'Usuario')
+        select('responsible',from: 'Rol')
+
+      end
+      click_button('Crear Task')
+      expect(page).to have_content('Task was successfully created.')
     end
   end
 end
